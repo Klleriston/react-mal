c8c630aacd81126f92156fb979ace4336dadfbe6
klleriston <klleandrade@outlook.com>
2024-10-29 11:27:51 -0300

    feat: dark mode on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import Header from "./components/Header";
 import Home from "./pages/Home";
+import { ThemeContext } from "./context/ThemeContext";
 
 export default function App() {
+  const [cont, setCount] = useState(0);
+  const [theme, setTheme] = useState('ligth')
   return (
-    <Home/>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`${theme} ${theme == 'dark' ? 'bg-[#121212]' : null} h-[100vh]`} >
+        <Header />
+        <Home />
+      </ div>
+    </ThemeContext.Provider>
+
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import MalLogo from '../assets/images/MalLogo.png'
 import { HiOutlineSearch, HiOutlineSun } from "react-icons/hi";
 import { HiOutlineMoon } from "react-icons/hi";
+import { ThemeContext } from '../context/ThemeContext';
 
 export default function Header() {
-    const [toggle, setToggle] = useState(true)
+    const [toggle, setToggle] = useState(true);
+    const { theme, setTheme } = useContext(ThemeContext);
+    
+    useEffect(() => {
+        console.log("Theme ", theme)
+    })
     return (
         <div className='flex items-center p-3'>
             <img src={MalLogo} width={60} height={60} className='rounded-full' />
@@ -13,8 +19,8 @@ export default function Header() {
                 <input type='text' className='bg-transparent outline-none px-2' placeholder='Busque um titulo'></input>
             </div>
             <div>
-                {toggle ? <HiOutlineMoon className='text-[45px] bg-slate-200 text-gray-500 p-1 rounded-full cursor-pointer' onClick={() => setToggle(!toggle)} /> :
-                    <HiOutlineSun className='text-[45px] bg-slate-200  text-gray-500 p-1 rounded-full cursor-pointer' onClick={() => setToggle(!toggle)} />}
+                {theme == 'ligth' ? <HiOutlineMoon className='text-[45px] bg-slate-200 text-gray-500 p-1 rounded-full cursor-pointer' onClick={() => setTheme('dark')} /> :
+                    <HiOutlineSun className='text-[45px] bg-slate-200  text-gray-500 p-1 rounded-full cursor-pointer' onClick={() => setTheme('ligth')} />}
             </div>
         </div>
     )
