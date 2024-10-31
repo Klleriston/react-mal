b15265ae83ad7862569c90e428034d83b4256422
klleriston <klleandrade@outlook.com>
2024-10-30 21:56:52 -0300

    feat: add hook for genre list by jika API

--- src/components/Genre.tsx
+++ src/components/Genre.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGenreList } from '../services/GlobalApi';
 
 export function Genre() {
+    const [genreList, setGenreList] = useState([]);
+    const [activeIndex, setActiveIndex] = useState(0)
     useEffect(() => {
         getGenreList()
             .then((resp) => {
-                console.log(resp.data.data);  
+                console.log(resp.data.data)
+                setGenreList(resp.data.data)
             })
             .catch((error) => {
                 console.error("Error fetching genres:", error);
@@ -14,7 +17,15 @@ export function Genre() {
 
     return (
         <div>
-            Genre List ----
-        </div>
+            <h2 className='text-[30px] font-bold dark:text-white'>Genre</h2>
+            {genreList.map((item, index) => (
+                <div
+                    onClick={() => setActiveIndex(index)}
+                    className={`flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg dark:hover:bg-gray-600 hover:scale-105 transition-all ease-out duratio-500 ${activeIndex == index ? 'bg-gray-600 dark:bg-gray-600' : null}`} >
+                    <h3 className={`dark:text-white houver:font-bolder transition-all ease-out duratio-500 ${activeIndex == index ? 'font-bold' : null}`}>{item.name}</h3>
+                </div>
+            ))
+            }
+        </div >
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import { Genre } from "../components/Genre";
 
 export default function Home() {
     return (
-        <div className="grid grid-cols-4">
-            <div className="bg-red-500 h-full hidden md:block">
+        <div className="grid grid-cols-4 px-8">
+            <div className="h-full hidden md:block">
             <Genre/>
             </div>
             <div className="col-span-4 md:col-span-3 bg-sky-400 h-full">Anime list</div>
